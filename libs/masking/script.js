2b19eaac9a1580eec45d87020fb5c8cb7f8b6dbb
DenisTM <yadakosten@yandex.ru>
2022-04-27 13:43:50 +0300

    commit

--- libs/masking/script.js
+++ libs/masking/script.js
@@ -17,7 +17,7 @@ masking = {
 	init: function () {
 		this.numberFormat = new Intl.NumberFormat();
 		for (let input of document.querySelectorAll('input, textarea, select')) {
-			input.addEventListener('change', this.inputValid);
+			input.addEventListener('change', this.inputValid.bind(this));
 			if (input.nodeName.toLowerCase() == 'select') {
 				this.setSelector(input);
 				document.addEventListener('click', (event) => {
@@ -30,56 +30,56 @@ masking = {
 				});
 			}
 			if (input.type == 'tel') {
-				input.addEventListener('input', this.onPhoneInput);
-				input.addEventListener('keydown', this.onPhoneKeydown);
+				input.addEventListener('input', this.onPhoneInput.bind(this));
+				input.addEventListener('keydown', this.onPhoneKeydown.bind(this));
 			}
 			if (input.type == 'number') {
 				input.type = 'tel';
 				input.old = 'number';
-				input.addEventListener('input', this.onNumericInput);
+				input.addEventListener('input', this.onNumericInput.bind(this));
 			}
 		}
 	},
 	setSelector: function (input) {
-		if (input.classList.contains(masking.selectorClasses.initialized)) return;
-		input.classList.add(masking.selectorClasses.initialized);
+		if (input.classList.contains(this.selectorClasses.initialized)) return;
+		input.classList.add(this.selectorClasses.initialized);
 		let select = input.cloneNode(true),
 			selectric = document.createElement('div'),
 			options = input.querySelectorAll('option');
-		selectric.classList.add(masking.selectorClasses.main);
-		selectric.innerHTML = `<div class="${masking.selectorClasses.input}"></div><div class="${masking.selectorClasses.label}"><div class="${masking.selectorClasses.title}"></div></div><ul class="${masking.selectorClasses.list}"></ul>`;
-		selectric.querySelector(`.${masking.selectorClasses.input}`).append(select);
-		let optionList = selectric.querySelector(`.${masking.selectorClasses['list']}`),
+		selectric.classList.add(this.selectorClasses.main);
+		selectric.innerHTML = `<div class="${this.selectorClasses.input}"></div><div class="${this.selectorClasses.label}"><div class="${this.selectorClasses.title}"></div></div><ul class="${this.selectorClasses.list}"></ul>`;
+		selectric.querySelector(`.${this.selectorClasses.input}`).append(select);
+		let optionList = selectric.querySelector(`.${this.selectorClasses['list']}`),
 			index = 0;
 		for (let option of options) {
 			if (option.selected) {
-				label = selectric.querySelector(`.${masking.selectorClasses.label}`);
-				title = label.querySelector(`.${masking.selectorClasses.title}`);
+				label = selectric.querySelector(`.${this.selectorClasses.label}`);
+				title = label.querySelector(`.${this.selectorClasses.title}`);
 				title.prepend(option.textContent);
 				let selectBtn = document.createElement('div');
-				selectBtn.classList.add(masking.selectorClasses.button);
+				selectBtn.classList.add(this.selectorClasses.button);
 				selectBtn.addEventListener('click', () => {
-					if (selectric.classList.contains(masking.selectorClasses.active)) {
-						selectric.classList.remove(masking.selectorClasses.active);
+					if (selectric.classList.contains(this.selectorClasses.active)) {
+						selectric.classList.remove(this.selectorClasses.active);
 						return;
 					}
-					selectric.classList.add(masking.selectorClasses.active);
-					let rect = selectric.querySelector(`.${masking.selectorClasses.label}`).getBoundingClientRect(),
-						listHeight = selectric.querySelector(`.${masking.selectorClasses.list}`).offsetHeight;
+					selectric.classList.add(this.selectorClasses.active);
+					let rect = selectric.querySelector(`.${this.selectorClasses.label}`).getBoundingClientRect(),
+						listHeight = selectric.querySelector(`.${this.selectorClasses.list}`).offsetHeight;
 					if (window.innerHeight - rect.top - rect.height < listHeight) {
-						selectric.classList.add(masking.selectorClasses.top);
+						selectric.classList.add(this.selectorClasses.top);
 					}
-					else selectric.classList.remove(masking.selectorClasses.top);
+					else selectric.classList.remove(this.selectorClasses.top);
 				});
 				label.append(selectBtn);
-				if (option.disabled) label.classList.add(masking.selectorClasses.disabled);
+				if (option.disabled) label.classList.add(this.selectorClasses.disabled);
 			}
 			let optionItem = document.createElement('li');
-			if (option.disabled) optionItem.classList.add(masking.selectorClasses.disabled);
-			if (option.selected) optionItem.classList.add(masking.selectorClasses.selected);
+			if (option.disabled) optionItem.classList.add(this.selectorClasses.disabled);
+			if (option.selected) optionItem.classList.add(this.selectorClasses.selected);
 			optionItem.setAttribute('data-index', index);
 			optionItem.innerHTML = option.textContent;
-			optionItem.addEventListener('click', masking.changeSelector);
+			optionItem.addEventListener('click', this.changeSelector.bind(this));
 			optionList.append(optionItem);
 			index++;
 		}
@@ -89,16 +89,16 @@ masking = {
 	changeSelector: function (event) {
 		let element = event.target,
 			idx = element.getAttribute('data-index'),
-			parent = element.closest(`.${masking.selectorClasses.main}`),
-			oldSelected = parent.querySelector(`li.${masking.selectorClasses.selected}`),
-			label = parent.querySelector(`.${masking.selectorClasses.title}`),
+			parent = element.closest(`.${this.selectorClasses.main}`),
+			oldSelected = parent.querySelector(`li.${this.selectorClasses.selected}`),
+			label = parent.querySelector(`.${this.selectorClasses.title}`),
 			oldIdx = oldSelected.getAttribute('data-index');
 		if (parent.getElementsByTagName('option')[idx].disabled == true) return;
-		oldSelected.classList.remove(masking.selectorClasses.selected);
+		oldSelected.classList.remove(this.selectorClasses.selected);
 		parent.getElementsByTagName('option')[oldIdx].selected = false;
 		parent.getElementsByTagName('option')[idx].selected = true;
-		element.classList.add(masking.selectorClasses.selected);
-		parent.classList.remove(masking.selectorClasses.active);
+		element.classList.add(this.selectorClasses.selected);
+		parent.classList.remove(this.selectorClasses.active);
 		label.innerHTML = element.textContent;
 	},
 	addStatus: function (status, element) {
@@ -119,17 +119,17 @@ masking = {
 		let element = event.target,
 			value = element.value,
 			required = element.required;
-		if (!value && required) return masking.addStatus(false, element);
+		if (!value && required) return this.addStatus(false, element);
 		else if (!value && !required) {
 			element.error = false;
 			if (element.nextSibling) element.nextSibling.remove();
 		}
 		else if (element.old == 'number') {
-			return (value.replace(/[\d ,\.\s]/g, '') == '') ? masking.addStatus(true, element) : masking.addStatus(false, element);
+			return (value.replace(/[\d ,\.\s]/g, '') == '') ? this.addStatus(true, element) : this.addStatus(false, element);
 		}
 		else if (element.type == 'email') {
 			let reg = /^((([0-9A-Za-z]{1}[-0-9A-z\.]{1,}[0-9A-Za-z]{1})|([0-9А-Яа-я]{1}[-0-9А-я\.]{1,}[0-9А-Яа-я]{1}))@([-A-Za-z]{1,}\.){1,2}[-A-Za-z]{2,})$/u;
-			return value.match(reg) ? masking.addStatus(true, element) : masking.addStatus(false, element);
+			return value.match(reg) ? this.addStatus(true, element) : this.addStatus(false, element);
 		}
 		else if (element.type == 'tel') {
 			let regs = [
@@ -144,11 +144,11 @@ masking = {
 					break;
 				}
 			}
-			return (phoneError) ? masking.addStatus(false, element) : masking.addStatus(true, element);
+			return (phoneError) ? this.addStatus(false, element) : this.addStatus(true, element);
 		}
 		else if (element.type == 'checkbox' || element.type == 'radio') return;
 		else {
-			return masking.addStatus(true, element);
+			return this.addStatus(true, element);
 		}
 	},
 	onNumericInput: function (event) {
@@ -158,25 +158,25 @@ masking = {
 		inputNumber = inputNumber.replace(/\s/g, '');
 		if (!inputNumber.match(/\d/g)) return input.value = '';
 		if (inputNumber.indexOf('.') != inputNumber.lastIndexOf('.')) return input.value = input.value.substring(0, input.value.length - 1);
-		if (!inputNumber.match(/\.$/g)) input.value = masking.numberFormat.format(inputNumber);
+		if (!inputNumber.match(/\.$/g)) input.value = this.numberFormat.format(inputNumber);
 	},
 	getInputNumbersValue: function (input) {
 		return input.value.trim().replace(/\D/g, '');
 	},
 	onPhoneKeydown: function (event) {
 		let input = event.target;
-		if (event.keyCode == 8 && masking.getInputNumbersValue(input).length == 1) input.value = '';
+		if (event.keyCode == 8 && this.getInputNumbersValue(input).length == 1) input.value = '';
 	},
 	onPhoneInput: function (event) {
 		let input = event.target,
-			inputNumbersValue = masking.getInputNumbersValue(input),
+			inputNumbersValue = this.getInputNumbersValue(input),
 			formattedInputValue = '';
 		if (!inputNumbersValue) {
 			return input.value = '';
 		}
 
-		if (input.selectionStart != input.value.length) masking.phonePosition = input.selectionStart;
-		else masking.phonePosition = false;
+		if (input.selectionStart != input.value.length) this.phonePosition = input.selectionStart;
+		else this.phonePosition = false;
 
 		if (['7', '8', '9'].includes(inputNumbersValue[0])) {
 			if (inputNumbersValue[0] == '9') inputNumbersValue = '7' + inputNumbersValue;
@@ -189,11 +189,11 @@ masking = {
 		}
 		else formattedInputValue = '+' + inputNumbersValue.substring(0, 16);
 		input.value = formattedInputValue;
-		if (masking.phonePosition && inputNumbersValue.length < 11) {
-			input.selectionStart = masking.phonePosition;
-			input.selectionEnd = masking.phonePosition;
+		if (this.phonePosition && inputNumbersValue.length < 11) {
+			input.selectionStart = this.phonePosition;
+			input.selectionEnd = this.phonePosition;
 		}
-		else masking.phonePosition = false;
+		else this.phonePosition = false;
 	}
 };
 
